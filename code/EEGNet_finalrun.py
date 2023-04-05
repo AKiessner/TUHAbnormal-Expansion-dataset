cb3671c798ff43894b8c7153885eea1890b8fc0d
AKiessner <65896642+AKiessner@users.noreply.github.com>
2023-04-05 18:18:04 +0200

    Create EEGNet_finalrun.py

---
+++ code/EEGNet_finalrun.py
@@ -0,0 +1,40 @@
+from train_tuheeg_pathology_balanced_saturation_finalrun import * 
+
+
+
+model_name = 'EEGNet'  # 
+
+
+drop_prob=0.25
+batch_size=64
+lr=0.001
+n_epochs=35
+weight_decay=0
+seed=20110407
+
+
+
+result_folder='/home/results/TUAB/eval/'
+train_folder = '/home/data/preprocessed_TUHAbnormal/final_train/'
+eval_folder = '/home/data/preprocessed_TUHAbnormal/final_eval/'
+
+    
+
+ids_to_load_train = None 
+
+task_name = 'TUAB_finalrun_' 
+
+train_TUHEEG_pathology(model_name=model_name,
+                     task_name = task_name,
+                     drop_prob=drop_prob,
+                     batch_size=batch_size,
+                     lr=lr,
+                     n_epochs=n_epochs,
+                     weight_decay=weight_decay,
+                     result_folder=result_folder,
+                     train_folder=train_folder,
+                     eval_folder=eval_folder,
+                     ids_to_load_train =  ids_to_load_train,
+                     cuda = True,
+                     seed= seed,
+                     )
